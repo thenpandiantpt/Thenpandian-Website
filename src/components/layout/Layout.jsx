6c98336c2eefbbd55cd63f6343757ed5542dc047
bharaninrb <bharaninrb2002@gmail.com>
2026-08-21 15:21:29 +0530

    Add Google Analytics

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 
 export default function Layout() {
+  useEffect(() => {
+    const measurementId = "G-LVWJLVWW4R";
+
+    // Prevent duplicate Google Analytics script
+    if (!document.getElementById("google-analytics")) {
+      const script = document.createElement("script");
+      script.id = "google-analytics";
+      script.async = true;
+      script.src = `https://www.googletagmanager.com/gtag/js?id=${measurementId}`;
+
+      document.head.appendChild(script);
+    }
+
+    window.dataLayer = window.dataLayer || [];
+
+    window.gtag = window.gtag || function () {
+      window.dataLayer.push(arguments);
+    };
+
+    window.gtag("js", new Date());
+    window.gtag("config", measurementId);
+  }, []);
+
   return (
     <div className="min-h-screen bg-background">
       <Navbar />
+
       <main>
         <Outlet />
       </main>
+
       <Footer />
     </div>
   );
